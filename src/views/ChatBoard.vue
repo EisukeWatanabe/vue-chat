2efc24ef0d420b3330a1a88bca9d5ffad3f01e98
渡邉英資 <awatanabe@fminc.xsrv.jp>
2022-08-16 17:18:01 +0900

    add submit message

--- src/views/ChatBoard.vue
+++ src/views/ChatBoard.vue
@@ -14,24 +14,21 @@
               <v-subheader>{{ card }}</v-subheader>
 
               <v-list two-line>
-                <template v-for="n in 6">
+                <template v-for="(data, index) in messages">
                   <v-list-item
-                      :key="n">
+                      :key="index">
                     <v-list-item-avatar color="grey darken-1">
                     </v-list-item-avatar>
 
                     <v-list-item-content>
-<!--                      <v-list-item-title>Message {{ n }}</v-list-item-title>-->
-
                       <v-list-item-subtitle class="message">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique
+                        {{ data.message }}
                       </v-list-item-subtitle>
                     </v-list-item-content>
                   </v-list-item>
 
                   <v-divider
-                      v-if="n !== 6"
-                      :key="`divider-${n}`"
+                      :key="`divider-${index}`"
                       inset>
                   </v-divider>
                 </template>
@@ -41,12 +38,23 @@
         </v-row>
       </v-container>
       <v-textarea
+          v-model="body"
           append-icon="mdi-comment"
           class="mx-2"
           label="メッセージを送信する"
           rows="3"
           auto-grow
       ></v-textarea>
+      <v-btn
+          class="mr-4"
+          type="submit"
+          :disabled="invalid"
+          @click="submit">
+        submit
+      </v-btn>
+      <v-btn @click="clear">
+        clear
+      </v-btn>
     </v-main>
   </v-app>
 </template>
@@ -58,6 +66,14 @@ export default {
     console.log("user_id", this.user_id)
   },
   data: () => ({
+    messages: [
+      {message: "message 1"},
+      {message: "message 2"},
+      {message: "message 3"},
+      {message: "message 4"},
+      {message: "message 5"}
+    ],
+    body: '',
     user_id: '',
     cards: ['Today'],
     drawer: null,
@@ -67,7 +83,22 @@ export default {
       ['mdi-delete', 'Trash'],
       ['mdi-alert-octagon', 'Spam'],
     ],
+    // invalid: false
   }),
+  computed: {
+    invalid() {
+      return !this.body;
+    }
+  },
+  methods: {
+    clear() {
+      this.body = ""
+    },
+    submit() {
+      this.messages.unshift({message: this.body});
+      this.body = "";
+    }
+  }
 }
 </script>
 
